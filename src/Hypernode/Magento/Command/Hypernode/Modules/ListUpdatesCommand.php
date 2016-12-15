8aa77b5b219291fb3e26e314ba1cb620d0f751cd
Fabio Ros <fabioros.it@gmail.com>
2016-12-15 15:36:09 +0100

    more information about inactive modules and refactoring

--- src/Hypernode/Magento/Command/Hypernode/Modules/ListUpdatesCommand.php
+++ src/Hypernode/Magento/Command/Hypernode/Modules/ListUpdatesCommand.php
@@ -33,24 +33,27 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
             ->addOption('codepool', null, InputOption::VALUE_OPTIONAL, 'Show modules in a specific codepool')
             ->addOption('status', null, InputOption::VALUE_OPTIONAL, 'Show modules with a specific status')
             ->addOption('vendor', null, InputOption::VALUE_OPTIONAL, 'Show modules of a specified vendor')
+            ->addOption('only-active', null, InputOption::VALUE_NONE, 'Ignore inactive modules')
             ->setDescription('Find available updates for installed modules.')
             ->addOption(
                 'format',
                 null,
                 InputOption::VALUE_OPTIONAL,
-                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+                'Output Format. One of ['.implode(',', RendererFactory::getFormats()).']'
             );
     }
 
     /**
-     * Get modules
-     *
+     * Returns an Object with Magento modules
+     * @param $input InputInterface
      * @return Modules
      */
-    public function getModules()
+    public function getModules($input)
     {
         $modules = new Modules();
-        return $modules->findInstalledModules();
+        $modules = $modules->findInstalledModules()->filterModules($input);
+
+        return $modules;
     }
 
     /**
@@ -65,8 +68,8 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
             return;
         }
 
-        $modules = $this->getModules()
-                ->filterModules($input);
+        $onlyActive = $input->getOption('only-active') ? true : false;
+        $modules = $this->getModules($input);
 
         if (!count($modules)) {
             $output->writeln('<error>Could not parse sys:modules:list</error>');
@@ -74,20 +77,28 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
         }
 
         // Compatibility fix
-        $listModules = array_map(function($item){
-            return array_combine(array('codePool', 'Name', 'Version', 'Status'), $item);
-        }, iterator_to_array($modules));
+        $listModules = array_map(
+            function ($item) {
+                return array_combine(array('codePool', 'Name', 'Version', 'Status'), $item);
+            },
+            iterator_to_array($modules)
+        );
 
         $options = JSON_FORCE_OBJECT;
         if (version_compare(PHP_VERSION, '5.4', '>=')) {
             $options |= JSON_PRETTY_PRINT;
         }
-        $listModulesJson = json_encode($listModules, $options);
 
         $modulesInfo = array();
         foreach ($listModules as $moduleInfo) {
+
+            if (!$moduleInfo['Version']) {
+                $moduleInfo['Version'] = $this->getExtensionVersion($moduleInfo['Name']);
+            }
             $modulesInfo[$moduleInfo['Name']] = $moduleInfo;
         }
+
+        $modulesInfoJson = json_encode($modulesInfo, $options);
 
         try {
 
@@ -95,13 +106,15 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
 
             $curl->setHeader('Accept', 'application/json');
             $curl->setHeader('Content-Type', 'application/json');
-            $curl->setHeader('Content-Length', strlen($listModulesJson));
+            $curl->setHeader('Content-Length', strlen($modulesInfoJson));
 
-            $curl->post(self::TOOLS_HYPERNODE_MODULE_URL, $listModulesJson);
+            $curl->post(self::TOOLS_HYPERNODE_MODULE_URL, $modulesInfoJson);
 
             $response = $curl->response;
-        } catch (Exception $e) {
-            $output->writeln('<error>Could not fetch data from Hypernode platform; ' . $e->getMessage() . '</error>');
+
+
+        } catch (\Exception $e) {
+            $output->writeln('<error>Could not fetch data from Hypernode platform; '.$e->getMessage().'</error>');
             exit(1);
         }
 
@@ -115,24 +128,32 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
 
         $rowsUpdates = $rowsLatest = array();
         foreach (array_pop($responseData) as $module) {
+
             $tableInfo = array_merge($module, $modulesInfo[$module['name']]);
 
-            $update = array($tableInfo['Name'], $tableInfo['codePool'], $tableInfo['current'], $tableInfo['latest']);
+            // inactive / active filtering
+            if ($tableInfo['Status'] === 'inactive' && $doFormat && !$onlyActive) {
+                $tableInfo['Name'] = '<error>'.$tableInfo['Name'].' (inactive)</error>';
+            } elseif ($onlyActive && $tableInfo['Status'] === 'inactive') {
+                continue;
+            }
 
-            $hasUpdate = $tableInfo['current'] != $tableInfo['latest'];
+            $update = array(
+                $tableInfo['Name'],
+                $tableInfo['codePool'],
+                $tableInfo['current'],
+                $tableInfo['latest'],
+                $tableInfo['Status'],
+            );
+
+            $hasUpdate = $tableInfo['current'] !== $tableInfo['latest'];
 
             if ($doFormat) {
-                $update[] = ($hasUpdate ? '<comment>Yes</comment>' : '<info>No</info>') . '';
+                $update[] = ($hasUpdate ? '<comment>Yes</comment>' : '<info>No</info>').'';
             } else {
                 $update[] = ($hasUpdate ? 'Yes' : 'No');
             }
 
-            /**
-             * No inactive modules are sent back from the Hypernode platform; maybe later?
-             */
-            /*if($tableInfo['Status'] == 'inactive') {
-                $tableInfo['Name'] .= ' <error>(inactive)</error>';
-            }*/
             if ($hasUpdate) {
                 $rowsUpdates[] = $update;
             } else {
@@ -143,8 +164,48 @@ class ListUpdatesCommand extends AbstractHypernodeCommand
         $rows = array_merge($rowsLatest, array(new TableSeparator()), $rowsUpdates);
 
         $this->getHelper('table')
-                ->setHeaders(array('Name', 'Code pool', 'Current version', 'Latest version', 'Newer version available?'))
-                ->renderByFormat($output, $rows, $input->getOption('format'));
+            ->setHeaders(
+                array('Name', 'Code pool', 'Current version', 'Latest version', 'Status', 'Newer version available?')
+            )
+            ->renderByFormat($output, $rows, $input->getOption('format'));
+    }
+
+    /**
+     * Finds out the version of a disabled / inactive module
+     *
+     * 1. Checks if the module version can be found in config (cache)
+     * 2. Else checks the modules' config.xml for the version
+     *
+     * @param $namespace
+     * @return bool|string|null
+     */
+    protected function getExtensionVersion($namespace)
+    {
+        /**
+         * Is it in config cache?
+         */
+        if ($versionFromCache = (string)\Mage::getConfig()->getNode()->modules->{$namespace}->version) {
+            return $versionFromCache;
+        }
+
+        $moduleDir = \Mage::getConfig()->getModuleDir('etc', $namespace);
+
+        if (file_exists($moduleDir)) {
+
+            $configFile = $moduleDir.DIRECTORY_SEPARATOR.'config.xml';
+
+            if ($xml = file_get_contents($configFile)) {
+
+                $config = new \Varien_Simplexml_Config($xml);
+
+                if ($version = $config->getNode('modules')->descend($namespace)->asArray()) {
+                    return $version['version'];
+                }
+            }
+        }
+
+        // returning null for version will exclude the module from getting version info
+        return null;
     }
 
 }
