8091f3e12790befa549170c0d463376f28b62d89
Lewis Voncken <lewis@experius.nl>
2018-10-25 11:41:28 +0200

    [TASK] Splitted Magento1 and Magento2 commands and added Crack_AdminPassword Magento 2 Command

---
+++ src/Hypernode/Magento2/Command/Hypernode/Log/ParseLogCommand.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Byte Hypernode Magerun
+ *
+ * @package     hypernode-Magerun
+ * @author      Byte
+ * @copyright   Copyright (c) 2016 Byte
+ * @license     http://opensource.org/licenses/osl-3.0.php Open Software License 3.0 (OSL-3.0)
+ */
+
+namespace Hypernode\Magento2\Command\Hypernode\Log;
+
+use Hypernode\Magento\Command\Hypernode\Log\ParseLogCommand as Magento1ParseLogCommand;
+
+/**
+ * Class ParseLogCommand
+ * @package Hypernode\Magento2\Command\Hypernode\Log
+ */
+class ParseLogCommand extends Magento1ParseLogCommand
+{
+
+}
